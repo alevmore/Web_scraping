8e1b76be7a64c997cf3ccc693fe703017ac379a9
Alev More <64901448+alevmore@users.noreply.github.com>
2024-11-05 18:06:32 +0200

    Update mongo.py

--- hw_3/quotes/mongo.py
+++ hw_3/quotes/mongo.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import json
 
-# Підключення до MongoDB
+# Connection to MongoDB
 client = MongoClient('mongodb://localhost:27017')
 db = client['quotes']
 collection = db['quotes']
 
-# Завантаження даних з файлу JSON та імпорт у колекцію
+# Downloading data from a JSON file and importing it into a collection
 with open('C:\Users\DELL\Desktop\data_analityka\hw_3\quotes\quotes.json', 'r') as file:
     data = json.load(file)
     collection.insert_many(data)
@@ -14,7 +14,7 @@ with open('C:\Users\DELL\Desktop\data_analityka\hw_3\quotes\quotes.json', 'r') a
 db = client['quotes']
 collection = db['authors']
 
-# Завантаження даних з файлу JSON та імпорт у колекцію
+# Downloading data from a JSON file and importing it into a collection
 with open('C:\Users\DELL\Desktop\data_analityka\hw_3\quotes\authors.json', 'r') as file_1:
     data_1 = json.load(file_1)
     collection.insert_many(data_1)
